88f6f9b9aa3cbc4d0211582f1521774b8ec02c42
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-23 23:26:36 +0500

    user auth created

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('exercises.urls'), name='exercises'),
+    path('api/', include('users.urls'), name='users'),
 ]
