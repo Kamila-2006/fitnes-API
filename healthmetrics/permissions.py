1feea55aec77d484999f5acf36c779854b12395c
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-31 18:56:07 +0500

    health-metrics created

--- healthmetrics/permissions.py
+++ healthmetrics/permissions.py
@@ -7,7 +7,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         
