9a479c881a03d3c7cc8be2f83634b666bce77d4b
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-31 17:33:07 +0500

    meals created

--- meals/serializers.py
+++ meals/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from .models import Meal, MealFood
+from foods.models import Food
 
 
 class MealSerializer(serializers.ModelSerializer):
+    foods = serializers.ListSerializer(child=serializers.DictField(), write_only=True)
+
     class Meta:
         model = Meal
         fields = ['id', 'date', 'meal_type', 'foods']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        meal_foods = MealFood.objects.filter(workout=instance)
+        meal_foods = MealFood.objects.filter(meal=instance)
 
         representation['foods'] = [
             {
@@ -23,8 +26,11 @@ class MealSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = self.context['request'].user
-        validated_data['user'] = user
+        foods_data = validated_data.pop('foods', [])
+        meal = Meal.objects.create(user=user, **validated_data)
 
-        meal = Meal.objects.create(**validated_data)
+        for item in foods_data:
+            food = Food.objects.get(id=item['food'])
+            MealFood.objects.create(meal=meal, food=food, quantity=item['quantity'])
 
         return meal
